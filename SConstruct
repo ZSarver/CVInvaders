80442099a0454cc69a32160c173400a263104c0e
Zachary Sarver <Zachary.Sarver@gmail.com>
2016-02-06 21:11:41 -0600

    Basics of the basics
    Set up a test main loop with a rotating image, so I know video is
    working properly. Gonna get started on text rendering next probably.

--- SConstruct
+++ SConstruct
@@ -1,4 +1,4 @@
 import os
 
-env = Environment(ENV=os.environ, tools=['mingw'], CPPPATH="C:\\SDL2-2.0.4\\include")
-env.Program('main.c', LIBPATH="C:\\SDL2-2.0.4\\lib\\x86", LIBS=['mingw32','SDL2'])
+env = Environment(ENV=os.environ, tools=['mingw'], CPPPATH="C:\\SDL2-2.0.4\\include", CCFLAGS="-Dmain=SDL_main")
+env.Program('cvinvaders.c', LIBPATH="C:\\SDL2-2.0.4\\lib\\x86", LIBS=['SDL2Main','SDL2', 'mingw32'])
---
+++ cvinvaders.c
@@ -0,0 +1,67 @@
+#include <stdio.h>
+#include "SDL.h"
+#include <stdbool.h>
+
+//int main(int nArg, char* args)
+int main(int argc, char *argv[])
+{
+
+  //initialization
+  if (SDL_Init(SDL_INIT_TIMER | SDL_INIT_AUDIO | SDL_INIT_VIDEO) != 0)
+    {
+      printf("oops you bwoke it \n SDL_Init failed \n");
+      return 1;
+    }
+  if (SDL_VideoInit(NULL) != 0)
+    {
+      printf("oops you bwoke it \n SDL_VideoInit failed \n");
+      return 1;
+    }
+
+  //create window
+  SDL_Window* window  = SDL_CreateWindow("test", 100, 100, 800, 600, 0);
+
+  //set up renderer
+  SDL_Renderer* rend  = SDL_CreateRenderer(window, -1,
+                                           SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
+
+  /* testing only */
+  //set up texture
+  SDL_Surface* surf = SDL_LoadBMP("test.bmp");
+  SDL_Texture*  tex = SDL_CreateTextureFromSurface(rend, surf);
+  SDL_FreeSurface(surf);
+  SDL_Rect rect;
+  rect.x = 350;
+  rect.y = 250;
+  rect.h = 200;
+  rect.w = 200;
+  
+
+  //dark grey, cooler than black
+  SDL_SetRenderDrawColor(rend, 130, 130, 130, 255);
+  //event loop
+  SDL_Event event;
+  bool shouldQuit = false;
+  double frame = 0.0;
+  while (!shouldQuit)
+    {
+      //let's draw some stuff
+      SDL_RenderClear(rend);
+      SDL_RenderCopyEx(rend, tex, NULL, &rect, frame, NULL, SDL_FLIP_NONE);
+      SDL_RenderPresent(rend);
+      
+      //get all the events this frame
+      while (SDL_PollEvent(&event))
+        {
+          if (event.type == SDL_QUIT)
+            {
+              shouldQuit = true;
+            }
+        }
+      frame = frame + 0.5;
+    }
+  
+  SDL_DestroyWindow(window);
+  SDL_Quit();
+  return 0;
+}
